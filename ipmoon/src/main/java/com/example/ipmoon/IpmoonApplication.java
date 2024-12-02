63109a25b52476c4e93d20c216661b0f208d5ee7
Local_Hwang <amongzamong@gmail.com>
2024-12-02 12:30:47 +0900

    Initial commit

---
+++ ipmoon/src/main/java/com/example/ipmoon/IpmoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.ipmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpmoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpmoonApplication.class, args);
+	}
+
+}
---
+++ ipmoon/src/test/java/com/example/ipmoon/IpmoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipmoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpmoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
