0cf35b4c961251e4ed32c17dfbb9be7ef97dae1a
Local_Hwang <amongzamong@gmail.com>
2024-12-02 13:21:45 +0900

    feat : Add hello-api

--- ipmoon/src/main/java/com/example/ipmoon/controller/HelloController.java
+++ ipmoon/src/main/java/com/example/ipmoon/controller/HelloController.java
@@ -3,15 +3,53 @@ package com.example.ipmoon.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     //Controller annotation 필수
     //Model을 통해 MVC 패턴
-    @GetMapping("Hello")
+
+    @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    //MVC pattern
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //데이터를 그대로 내려줌
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    //Json 방식으로(key[name]-value[RequestParam]) 객체로 반환하면 자동으로 객체가 Json으로 반환됨
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name; //Method로 접근
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
